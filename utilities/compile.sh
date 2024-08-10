2ed53e251505c8bc33fdbf0a434e26f121a13f6e
Orion Jamal <orionjamal@gmail.com>
2024-08-09 18:35:16 -0700

    Added compatbility with macOS and others for html. Other fixes soon.

--- utilities/compile.sh
+++ utilities/compile.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 read -p "Enter file name (include extension): " FILE
-
+OS=$(uname -s)
 
 case "$FILE" in
 	*.cpp)
+		echo "File Type: C++"
 		g++ $FILE
 		./a.out
 	;;
 
 	*.py)
+		echo "File Type: Python"
 		python3 $FILE
 	;;
 
 	*.java)
-
+		
+		echo "File Type: Java"
 		javac $FILE
 		java $FILE
 	;;
 
 	*.html)
-		xdg-open $FILE
+		echo "File Type: HTML/CSS/JS"
+		if [ "$OS" = Linux ]; then
+			xdg-open $FILE
+		else
+			open $FILE
+		fi
+		
+
 	;;	
 esac
 #Trying out env variables
